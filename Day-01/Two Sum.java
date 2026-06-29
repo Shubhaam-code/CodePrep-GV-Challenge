29116644776da2c401a61eed2af7d248aafd60a8
Shubham Naryan <shubhamnarayan11144@gmail.com>
2026-06-29 11:48:39 +0530

    Add solution for Two Sum (Auto-sync)

---
+++ Day-01/Two Sum.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // Map to store value: index
+        Map<Integer, Integer> numMap = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            // Check if complement is already seen
+            if (numMap.containsKey(complement)) {
+                return new int[] { numMap.get(complement), i };
+            }
+            
+            // Store the current index
+            numMap.put(nums[i], i);
+        }
+        
+        return new int[] {}; // Return empty if no solution
+    }
+}
